6bde476077900b7b6dfd5d1ebd12f297f418a6d2
Raj Mistry <rajm.albiorix@gmail.com>
2025-06-21 23:08:39 +0530

    Added Cors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://compass-insurance-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
